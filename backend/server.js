90a9eda1692da1d3330c037ea09f35ef4af06634
GOGUL <gogulmahadevan1124@gmail.com>
2026-06-12 16:49:19 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/leads", require("./routes/leads"));
+
+app.get("/", (req, res) => {
+  res.send("CRM Backend Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
